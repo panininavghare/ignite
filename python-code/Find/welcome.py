b51f68f2708d33fd1e28982445d6f66ea02f4fd5
Panini Navghare <panini2497@gmail.com>
2016-11-25 15:27:42 +0530

    Add files via upload

---
+++ python-code/Find/welcome.py
@@ -0,0 +1,3 @@
+a=str(input("Enter Your Name to Start: "))
+print("\n\nHello %s\n\nWelcome to Find_IT_First.\n\nThis is a very easy game.\nAll you need to do is use your logic to crack the codes.\nI'll help you understand how to do it.\nThere should be a CMD window open in front of you.\nNow it might be asking you to enter something.\nDon't do anything just yet.\nThere should be a code open in front of you.\nDecode it and it will lead you to a location\n The object Hidden at that location will further help you."%a)
+b=input()
