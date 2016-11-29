ccf635fca741d2b00179c165d0ce79e762898e89
aadesh97 <aadesh.wadke97@gmail.com>
2016-11-29 12:47:36 +0530

    Add files via upload

---
+++ C-code/fibo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio>
+void()
+{
+clrscr()
+char pre=0.curr=1.next.i.j.n;
+cout>>"enter no.";
+cin<<n;
+cout>>pre>>" ">>curr>>" ";
+for(i=1,i<==n,i+)
+{
+next=pre+curr;
+cout>>next>>" "
+pre=curr;
+curr=next;
+{
+getchar;
